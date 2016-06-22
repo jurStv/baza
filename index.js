9f420aad700cc751b0717ccfae6e6e84a63a32d0
Jura Studenkov <jurstv@gmail.com>
2016-06-22 23:23:54 +0300

    #

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 var express = require('express')
     , morgan = require('morgan')
+    , path = require('path')
     , app = express()
     , port = process.env.PORT || 3000
     , router = express.Router();
 
-app.use(express.static(__dirname + '/public')); // set the static files location /public/img will be /img for users
-app.use(morgan('dev'));                     // log every request to the console
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(morgan('dev'));
+app.set('views', __dirname + '/views');
+app.engine('html', require('ejs').renderFile);
+app.set('view engine', 'html');
 
 router.get('/', function(req, res, next) {
     res.render('index.html');

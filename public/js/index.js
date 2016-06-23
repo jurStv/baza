2a743967472b29fb8eed52cefec99e5129b2f609
Jura Studenkov <jurstv@gmail.com>
2016-06-23 10:20:42 +0300

    fixed smth

--- public/js/index.js
+++ public/js/index.js
@@ -1,24 +1,30 @@
 $(document).ready(function(){
   var video = $('#zzzzzz')
   var audio = $('#xxxxxx')
+  var first = false
   video.on('canplaythrough', function(){
-    setTimeout(function(){
-      video.get(0).play()
-      video.get(0).playbackRate = 0.5;
-      audio.get(0).volume = 0;
-      var int = setInterval(function(){
-        try {
-          audio.get(0).volume += 0.1
-        } catch (e) {
-          audio.get(0).volume = 1;
-          clearInterval(int)
-        }
-      }, 200)
-      audio.get(0).play()
-      $('#loading-modal').animate({opacity: 0}, {duration: 700, complete: function(){
-        $('#loading-modal').hide();
-      }});
-    }, 2000)
+    console.log(video.get(0).playbackRate)
+    if (!first) {
+      setTimeout(function(){
+        video.get(0).play()
+        video.get(0).playbackRate = 0.5;
+        audio.get(0).volume = 0;
+        var int = setInterval(function(){
+          try {
+            audio.get(0).volume += 0.1
+          } catch (e) {
+            audio.get(0).volume = 1;
+            clearInterval(int)
+          }
+        }, 200)
+        audio.get(0).play()
+        $('#loading-modal').animate({opacity: 0}, {duration: 700, complete: function(){
+          $('#loading-modal').hide();
+        }});
+      }, 2000);
+      first = true
+    }
+
   });
 
   function second_passed() {
